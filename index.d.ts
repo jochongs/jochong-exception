7c832f02cd96a08b56bc837d896bedc97999ba18
jochongs <joch2712@naver.com>
2024-01-20 15:38:32 +0900

    feat: modify all Exception class constructor

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,37 @@
-type Err = any
+type Err = any;
 
 export class Exception {
-    constructor(status: number, name: string, message: string, err: Err, response: object)
+    constructor(status: number, message: string, err?: Err);
 
     public err: Err;
     public status: number;
     public message: string;
-    public name: string;
-    public response: object;
 }
 
 export class BadRequestException extends Exception {
-    constructor(err: Err, message?: string, defaultMessageOption?: boolean, response?: object);
+    constructor(message: string, err?: Err);
 }
 
 export class UnauthorizedException extends Exception {
-    constructor(err: Err, message?: string, defaultMessageOption?: boolean, response?: object);
+    constructor(message: string, err?: Err);
 }
 
 export class ForbiddenException extends Exception {
-    constructor(err: Err, message?: string, defaultMessageOption?: boolean, response?: object);
+    constructor(message: string, err?: Err);
 }
 
 export class NotFoundException extends Exception {
-    constructor(err: Err, message?: string, defaultMessageOption?: boolean, response?: object);
+    constructor(message: string, err?: Err);
 }
 
 export class ConflictException extends Exception {
-    constructor(err: Err, message?: string, defaultMessageOption?: boolean, response?: object);
+    constructor(message: string, err?: Err);
 }
 
 export class InternalServerErrorException extends Exception {
-    constructor(err: Err, message?: string, defaultMessageOption?: boolean, response?: object);
+    constructor(message: string, err?: Err);
+}
+
+export class HttpException extends Exception {
+    constructor(status: number, message: string, err?: Err);
 }
